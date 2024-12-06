08a31a7111390e87474b5a51bf66a97df41822d7
Zack Chrisman <zjchrisman@student.fullsail.edu>
2024-12-06 09:34:32 -0600

    finished assignment

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const router = require("./routes/index");
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
+app.use(router);
