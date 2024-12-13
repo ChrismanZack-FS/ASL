77d926ba06529ad2d123a64b95560e21beeb8b4b
Zack Chrisman <zjchrisman@student.fullsail.edu>
2024-12-13 13:55:23 -0600

    finished StarTracker

---
+++ controllers/planet.js
@@ -0,0 +1,41 @@
+const {Planet,Star} = require("../models/index")
+
+// Show all resources
+const index = async (req, res) => {
+  const planets = await Planet.findAll({include:[Star]})
+  // Respond with an array and 2xx status code
+  res.status(200).json(planets)
+}
+
+// Show resource
+const show = async (req, res) => {
+  const planet = await Planet.findByPk(req.params.id,{include:[Star]})
+  // Respond with a single object and 2xx code
+  res.status(200).json(planet)
+}
+
+// Create a new resource
+const create = async (req, res) => {
+  const planet = await Planet.create(req.body)
+  
+  res.status(201).json(planet)
+  // Issue a redirect with a success 2xx code
+  //res.redirect(`/planets`, 201)
+}
+
+// Update an existing resource
+const update = async (req, res) => {
+  const planet = await Planet.update(req.body, { where: { id: req.params.id} })
+  // Respond with a single resource and 2xx code
+  res.status(200).json(planet )
+}
+
+// Remove a single resource
+const remove =  async(req, res) => {
+  Planet.destroy( { where: { id: req.params.id} } )
+  // Respond with a 2xx status code and bool
+  res.status(204).json(true)
+}
+
+// Export all controller actions
+module.exports = { index, show, create, update, remove }
---
+++ controllers/star.js
@@ -0,0 +1,40 @@
+const {Star,Planet,Galaxy} = require("../models/index")
+
+// Show all resources
+const index = async (req, res) => {
+  const stars = await Star.findAll({include:[Planet,Galaxy]})
+  // Respond with an array and 2xx status code
+  res.status(200).json(stars)
+}
+
+// Show resource
+const show = async (req, res) => {
+  const star = await Star.findByPk(req.params.id,{include:[Planet,Galaxy]})
+  // Respond with a single object and 2xx code
+  res.status(200).json(star)
+}
+
+// Create a new resource
+const create = async (req, res) => {
+  const star = await Star.create(req.body)
+  res.status(201).json(star)
+  // Issue a redirect with a success 2xx code
+  //res.redirect(`/stars`, 201)
+}
+
+// Update an existing resource
+const update = async (req, res) => {
+  const star = await Star.update(req.body,{where: {id: req.params.id}})
+  // Respond with a single resource and 2xx code
+  res.status(200).json(star)
+}
+
+// Remove a single resource
+const remove = async (req, res) => {
+    Star.destroy( { where: { id: req.params.id} } )
+  // Respond with a 2xx status code and bool
+  res.status(204).json(true)
+}
+
+// Export all controller actions
+module.exports = { index, show, create, update, remove }
